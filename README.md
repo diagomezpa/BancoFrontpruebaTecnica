a3efdcd7ad6eeb09e0628ef75bc96af5b3eb8bf4
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-08-26 09:29:15 -0500

    modificando readme

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.3.
 
+## Java Requirement
+
+To execute the test, you must have Java 17 or later installed.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
@@ -18,11 +22,17 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+## Banco de Bogotá
+
+This technical test was carried out for Banco de Bogotá.
+
+## Simultaneous Execution
+
+For the project to function correctly, it must be run simultaneously with the backend project available at [this URL](https://github.com/diagomezpa/BackPruebatecnica.git).
+
 
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
-# BancoFrontpruebaTecnica
+
--- src/app/clients/pages/clients/clients.component.spec.ts
+++ src/app/clients/pages/clients/clients.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ClientsComponent } from './clients.component';
 
 describe('ClientsComponent', () => {
@@ -8,10 +9,13 @@ describe('ClientsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ClientsComponent]
+      declarations: [ ClientsComponent ],
+      imports: [ ReactiveFormsModule,HttpClientTestingModule ]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ClientsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +24,33 @@ describe('ClientsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+    it('should have a form with documentType and numberDocument controls', () => {
+      expect(component.myForm.contains('documentType')).toBeTruthy();
+      expect(component.myForm.contains('numberDocument')).toBeTruthy();
+  });
+
+  it('should make the documentType control required', () => {
+    const control = component.myForm.get('documentType');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
+
+    it('should make the numberDocument control required and validate length', () => {
+    const control = component.myForm.get('numberDocument');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+
+    control?.setValue('1234567'); // Less than 8 characters
+    expect(control?.valid).toBeFalsy();
+
+    control?.setValue('12345678'); // Valid length
+    expect(control?.valid).toBeTruthy();
+
+    control?.setValue('123456789012'); // More than 11 characters
+    expect(control?.valid).toBeFalsy();
+  });
+
+
+
 });
